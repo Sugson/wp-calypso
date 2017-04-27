5ffe049aaae1320e8f641c10551fa8fff32acd0b
Donna Peplinskie <donnapep@gmail.com>
2017-04-27 14:49:46 -0400

    Add Redux actions, reducers and selectors for testing the cache

---
+++ client/extensions/wp-super-cache/state/cache-test/actions.js
@@ -0,0 +1,49 @@
+/**
+ * Internal dependencies
+ */
+import wp from 'lib/wp';
+import {
+	WP_SUPER_CACHE_RECEIVE_TEST_CACHE_RESULTS,
+	WP_SUPER_CACHE_TEST_CACHE,
+	WP_SUPER_CACHE_TEST_CACHE_FAILURE,
+	WP_SUPER_CACHE_TEST_CACHE_SUCCESS,
+} from '../action-types';
+
+/**
+ * Returns an action object to be used in signalling that cache test results have been received.
+ *
+ * @param  {Number} siteId Site ID
+ * @param  {Object} data Cache test results object
+ * @return {Object} Action object
+ */
+export const receiveResults = ( siteId, data ) => ( { type: WP_SUPER_CACHE_RECEIVE_TEST_CACHE_RESULTS, siteId, data } );
+
+/*
+ * Tests the cache for a site.
+ *
+ * @param  {Number} siteId Site ID
+ * @returns {Function} Action thunk that tests the cache ror a given site
+ */
+export const testCache = ( siteId, httpOnly ) => {
+	return ( dispatch ) => {
+		dispatch( {
+			type: WP_SUPER_CACHE_TEST_CACHE,
+			siteId,
+		} );
+
+		return wp.req.post( { path: `/jetpack-blogs/${ siteId }/rest-api/` }, {}, { path: '/wp-super-cache/v1/cache/test', httpOnly } )
+			.then( ( { data } ) => {
+				dispatch( receiveResults( siteId, data ) );
+				dispatch( {
+					type: WP_SUPER_CACHE_TEST_CACHE_SUCCESS,
+					siteId,
+				} );
+			} )
+			.catch( () => {
+				dispatch( {
+					type: WP_SUPER_CACHE_TEST_CACHE_FAILURE,
+					siteId,
+				} );
+			} );
+	};
+};
